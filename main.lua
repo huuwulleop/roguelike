5f8b530d66cac4bbe4387f5ddc5e2c43673f16bf
VP <huuwulleop@hotmail.com>
2022-05-02 08:06:37 -0400

    Add player movement

--- main.lua
+++ main.lua
@@ -2,6 +2,7 @@
 function _init()
     _upd = update_game
     _drw = draw_game
+    start_game()
 end
 
 function _update()
@@ -13,6 +14,7 @@ function _draw()
 end
 
 function start_game()
+    -- player coord
     p_x = 11
     p_y = 6
 end
@@ -21,8 +23,18 @@ end
 
 -- UPDATES
 function update_game()
-    cls(0)
-    map()
+    if btn(0) then
+        p_x -= 1
+    end
+    if btn(1) then
+        p_x += 1
+    end
+    if btn(2) then
+        p_y -= 1
+    end
+    if btn(3) then
+        p_y += 1
+    end
 end
 
 function update_gameover()
@@ -33,7 +45,14 @@ end
 
 -- DRAWS
 function draw_game()
+    cls(0)
+    map()
 
+    palt(0, false)
+    -- change color to yel
+    pal(6, 10)
+    spr(240, p_x * 8, p_y * 8)
+    pal()
 end
 
 function draw_gameover()
