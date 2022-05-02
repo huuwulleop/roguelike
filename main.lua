8f27869bf3960c283c0e79b7fd215bf3c09901b7
VP <huuwulleop@hotmail.com>
2022-05-02 09:37:45 -0400

    Add turning direction

--- main.lua
+++ main.lua
@@ -29,6 +29,8 @@ function start_game()
     p_ox, p_oy = 0,0
     p_sox, p_soy = 0,0
     p_timer = 0
+    -- flipping
+    p_flip = false
 end
 
 
@@ -39,6 +41,11 @@ function update_game()
     for i = 0,3 do
         if btnp(i) then
             local dx,dy = dir_x[i+1],dir_y[i+1]
+            if dx < 0 then
+                p_flip = true
+            elseif dx > 0 then
+                p_flip = false
+            end
             p_x += dx
             p_y += dy
             p_sox, p_soy = -dx*8, -dy*8
@@ -52,6 +59,7 @@ end
 
 -- move anim
 function update_pturn()
+    -- change speed
     p_timer = min(p_timer + 0.2, 1)
 
     p_ox = p_sox * (1 - p_timer)
@@ -74,12 +82,13 @@ function draw_game()
     cls(0)
     map()
 
-    draw_spr(get_frame(p_anim), p_x * 8 + p_ox, p_y * 8 + p_oy, 10)
+    draw_spr(get_frame(p_anim), p_x * 8 + p_ox, p_y * 8 + p_oy, 10, p_flip)
 end
 
 function draw_gameover()
 
 end
+
 
 ----------
 -- TOOLS
@@ -88,9 +97,14 @@ function get_frame(anim)
     return anim[flr(time/8)% #anim + 1]
 end
 
-function draw_spr(_spr,_x,_y,_c)
+function draw_spr(_spr,_x,_y,_c,_flip)
     palt(0,false)
     pal(6,_c) --change color
-    spr(_spr,_x,_y)
+    spr(_spr,_x,_y,1,1,_flip)
     pal() --reset
 end
+
+
+-------------
+-- GAMEPLAY
+-------------
